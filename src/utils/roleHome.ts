9a9912fe74f109d200946ca305adbf7a48f96caf
Sanjay Sakthivel <sanjay.sakthivel@aspiresys.com>
2025-12-02 15:48:29 +0530

    updated

--- src/utils/roleHome.ts
+++ src/utils/roleHome.ts
@@ -1,5 +1,5 @@
 export const ROLE_HOME: Record<string, string> = {
-  employee: "/employee/dashboard",
+  employee: "/employee/form",
   "reporting manager": "/manager/dashboard",
   "associate manager": "/associate/dashboard",
   VP: "/vp/dashboard",
