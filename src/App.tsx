b198572be11224949c335b54abcc1a2d9213f20b
Sanjay Sakthivel <sanjay.sakthivel@aspiresys.com>
2025-12-15 10:41:13 +0530

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -5,35 +5,44 @@ import {
   Navigate,
   useLocation,
 } from "react-router-dom";
-import Login from "./components/LoginPage";
-import Signup from "./components/SignupPage";
-import PublicRoute from "./routes/PublicRoute";
-import Unauthorized from "./pages/Unauthorized";
-import { routeConfig } from "./routes/RouteConfig";
-import ProtectedLayout from "./routes/ProtectedLayout";
-import Navbar from "./components/common/Navbar";
-import WelcomePage from "./components/WelcomePage";
-
-import { useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { loadUser } from "./store/actions/authActions";
+import { useDispatch } from "react-redux";
 import type { ThunkDispatch } from "redux-thunk";
 import type { RootState } from "./store";
 import type { AnyAction } from "redux";
 
+import Login from "./components/LoginPage";
+import Signup from "./components/SignupPage";
+import WelcomePage from "./components/WelcomePage";
+import Navbar from "./components/common/Navbar";
+import Unauthorized from "./pages/Unauthorized";
+
+import PublicRoute from "./routes/PublicRoute";
+import ProtectedLayout from "./routes/ProtectedLayout";
+import { routeConfig } from "./routes/RouteConfig";
+import { loadUser } from "./store/actions/authActions";
+
+import "./App.css";
+
+/* =========================
+   App Content (Routes)
+   ========================= */
+
 function AppContent() {
   const location = useLocation();
-  // const { showMessage } = useSnackbar();
 
-  const hideNavbarRoutes = ["/"]; // Hide navbar on welcome page
+  // Hide navbar only on welcome page
+  const hideNavbarRoutes = ["/"];
 
   return (
     <>
       {!hideNavbarRoutes.includes(location.pathname) && <Navbar />}
 
       <Routes>
+        {/* Welcome */}
         <Route path="/" element={<WelcomePage />} />
 
+        {/* Public Routes */}
         <Route
           path="/login"
           element={
@@ -52,9 +61,12 @@ function AppContent() {
           }
         />
 
-        {/* Dynamic Role Routes */}
+        {/* Role-based Protected Routes */}
         {Object.entries(routeConfig).map(([role, routes]) => (
-          <Route key={role} element={<ProtectedLayout allowedRoles={[role]} />}>
+          <Route
+            key={role}
+            element={<ProtectedLayout allowedRoles={[role]} />}
+          >
             {routes.map((route) => (
               <Route
                 key={route.path}
@@ -65,15 +77,21 @@ function AppContent() {
           </Route>
         ))}
 
+        {/* Unauthorized */}
         <Route path="/unauthorized" element={<Unauthorized />} />
 
+        {/* Fallback */}
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </>
   );
 }
 
-type AppDispatch = ThunkDispatch<RootState, any, AnyAction>;
+/* =========================
+   App Root
+   ========================= */
+
+type AppDispatch = ThunkDispatch<RootState, unknown, AnyAction>;
 
 export default function App() {
   const dispatch: AppDispatch = useDispatch();
